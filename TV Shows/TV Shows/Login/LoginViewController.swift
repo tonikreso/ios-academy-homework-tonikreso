57c253cd253aa88736c4f2a521d9788862d03858
tonikreso <tonikreso2105@gmail.com>
2021-07-21 06:44:34 +0200

    Solved most of hw02

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -9,55 +9,79 @@ import Foundation
 import UIKit
 
 final class LoginViewController: UIViewController {
-    @IBOutlet private weak var counterButton: UIButton!
-    @IBOutlet private weak var counterLabel: UILabel!
-    @IBOutlet private weak var activityIndicatorView: UIActivityIndicatorView!
     
-    private var numberOfTaps = 0
-    private var stopActivityIndicatorDispatch: DispatchWorkItem!
+    @IBOutlet private weak var horizontalStackView: UIStackView!
+    @IBOutlet private weak var verticalStackView: UIStackView!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var emailLineView: UIView!
+    @IBOutlet private weak var passwordLineView: UIView!
+    @IBOutlet private weak var rememberMeButton: UIButton!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var registerButton: UIButton!
     
-    @IBAction func buttonTouchUpInside(_ sender: Any) {
-        numberOfTaps += 1
-        counterLabel.text = "The button has been tapped \(numberOfTaps) times!"
-        
-        //every time the button is pressed if indicator is spinning stop it,
-        //if not, start spinning for 3 seconds
-        if !stopActivityIndicatorDispatch.isCancelled {
-            stopAndCancelActivityIndicator()
-        } else {
-            createTimerAndShowActivityIndicator()
-        }
-    }
-    
-    
+    private var passwordButton: UIButton!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        setupTapCounterLabel()
-        createTimerAndShowActivityIndicator()
+        setupTextFields()
+        setupButtons()
+        
     }
     
-    func createDispatchWorkItem() {
-        //create new DispatchWorkItem every time the countdown starts
-        stopActivityIndicatorDispatch = DispatchWorkItem { [weak self] in
-            self?.stopAndCancelActivityIndicator()
+    private func setupButtons() {
+        loginButton.layer.cornerRadius = 25
+        loginButton.setTitleColor(.lightText, for: .normal)
+        registerButton.setTitleColor(.lightText, for: .normal)
+    }
+    
+    private func setupTextFields() {
+        passwordButton = UIButton()
+        passwordButton.setImage(UIImage(named: "visibility-icon-open"), for: .normal)
+        passwordTextField.rightViewMode = .whileEditing
+        passwordTextField.rightView = passwordButton
+        passwordButton.addTarget(self, action: #selector(refreshPasswordVisibility), for: .touchUpInside)
+        
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightText])
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightText])
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    @IBAction func rememberMeAction(_ sender: Any) {
+        if rememberMeButton.tag == 0 {
+            rememberMeButton.tag = 1
+            rememberMeButton.setImage( UIImage(named: "ic-checkbox-selected"), for: .normal)
+        } else {
+            rememberMeButton.tag = 0
+            rememberMeButton.setImage( UIImage(named: "ic-checkbox-unselected"), for: .normal)
+        }
+    }
+    @IBAction func refreshPasswordVisibility(_ sender: Any) {
+        passwordTextField.isSecureTextEntry.toggle()
+        if passwordTextField.isSecureTextEntry {
+            passwordButton.setImage(UIImage(named: "visibility-icon-open"), for: .normal)
+        } else {
+            passwordButton.setImage(UIImage(named: "visibility-icon-closed"), for: .normal)
         }
     }
     
-    func createTimerAndShowActivityIndicator() {
-        createDispatchWorkItem()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: stopActivityIndicatorDispatch)
-        activityIndicatorView.startAnimating()
-    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
     
-    func setupTapCounterLabel() {
-        counterLabel.text = "Please tap the button."
-        counterLabel.layer.cornerRadius = 10
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if emailTextField.text != "" && passwordTextField.text != "" {
+            //once the button becomes active it will not become inactive again
+            //even if user empties a text field
+            //delegates are removed so code doesn't run for nothing
+            emailTextField.delegate = nil
+            passwordTextField.delegate = nil
+            
+            loginButton.backgroundColor = .white
+            let color = UIColor(red: 82/255, green: 54/255, blue: 140/255, alpha: 1)
+            loginButton.setTitleColor(color, for: .normal)
+            registerButton.setTitleColor(.white, for: .normal)
+        }
     }
-    
-    func stopAndCancelActivityIndicator() {
-        stopActivityIndicatorDispatch.cancel()
-        activityIndicatorView.stopAnimating()
-    }
-    
 }
